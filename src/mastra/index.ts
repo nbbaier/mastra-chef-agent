cc076682da25e76b5ec37a0d237b8bfd7a755f82
Nico Baier <nico.baier@gmail.com>
2025-07-25 15:05:02 -0500

    removed memory

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,6 +1,5 @@
 import { Mastra } from "@mastra/core/mastra";
 import { VercelDeployer } from "@mastra/deployer-vercel";
-import { LibSQLStore } from "@mastra/libsql";
 import { PinoLogger } from "@mastra/loggers";
 import { chefAgent } from "./agents/chef-agent";
 import { weatherAgent } from "./agents/weather-agent";
@@ -9,10 +8,10 @@ import { weatherWorkflow } from "./workflows/weather-workflow";
 export const mastra = new Mastra({
 	workflows: { weatherWorkflow },
 	agents: { weatherAgent, chefAgent },
-	storage: new LibSQLStore({
-		// stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-		url: ":memory:",
-	}),
+	// storage: new LibSQLStore({
+	// 	// stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+	// 	url: ":memory:",
+	// }),
 	logger: new PinoLogger({
 		name: "Mastra",
 		level: "info",
